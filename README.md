afdde641188703e1f8510682040430cd3ab1ca2d
Tyler <70815823+T-Syme@users.noreply.github.com>
2022-07-02 10:28:29 -0400

    Add cars to team page

--- README.md
+++ README.md
@@ -30,7 +30,13 @@ This is a vue project with vue-router following the standard file structure. Whe
 
 ## Automation
 
-The scripts in the automation folder should make it simple to update the content for the people on the team page. To update them:
+The scripts in the automation folder should make it simple to update the content for the people and cars on the team page.  
+
+To update the people:
 1. Save as the excel file as `people.xls` in the automation folder (the name can be changed in `people.py` but the extension .xls must stay the same). Note that everyone will appear in the site in the same order as in the file.
 2. Run the python script using `python people.py > people.json`. This will create/update the people.json file in the automation folder. The UI will import that file and generate the associated code.
 3. Add each person's image to the folder `assets/images/people` with the format `Firstname.jpg`. The UI will automatically import them.
+
+Any changes made directly to the `people.json` will be displayed immediately, but if the `people.xls` file is modified and the python script is re-run, then those changes may be overwritten.  
+
+To update the cars, add the car info to the `cars.json` file in the automation folder and add the corresponding image to the folder `assets/images/cars` with the format `year.jpg` (ex: MB22.jpg). In the json file, add an object for the car like this: `{ "year": "MB18", "name": "Tilly" }`. If it doesn't have a name, just use an empty string (so instead of "Tilly", it would be "").
--- src/views/TeamView.vue
+++ src/views/TeamView.vue
@@ -1,5 +1,6 @@
 <script setup>
 import { people } from '@/assets/automation/people.json'
+import { cars } from '@/assets/automation/cars.json'
 </script>
 
 <template>
@@ -30,13 +31,39 @@ import { people } from '@/assets/automation/people.json'
       <div class="row row-cols-1 row-cols-md-2 row-cols-lg-3 row-cols-xl-4 pt-3">
         <div class="col person p-3" v-for="person in people">
           <div class="h-100 text-center px-3 pt-5 pb-3 mx-auto">
-            <div class="person-img w-100">
-              <img :src="'src/assets/images/people/' + person.name.split(' ')[0] + '.jpg'" :alt="person.name" />
+            <div class="person-img w-100 d-flex justify-content-center align-items-center">
+              <img :src="'src/assets/images/people/' + person.name.split(' ')[0] + '.jpg'" :alt="'I\'m ' + person.name + ' and I don\'t have a picture :('" />
             </div>
             <div class="pt-3">
               <h5 class="py-3">{{person.name}}</h5>
               <p>{{person.role}}</p>
               <p>{{person.department}}, {{person.year}}</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section id="cars" class="container-xl py-3 py-lg-5">
+      <div class="text-center py-5">
+        <h2 class="display-3">Our Cars</h2>
+        <div class="title-separator mt-3 mb-5 mx-auto"></div>
+        <div class="container">
+          <p class="fs-5 px-4">
+            These are the cars created by the team over the years
+          </p>
+        </div>
+      </div>
+
+      <div class="row row-cols-1 row-cols-md-2 row-cols-lg-3 pt-3">
+        <div class="col car p-3" v-for="car in cars">
+          <div class="h-100 text-center px-4 pt-4 pb-3 mx-auto">
+            <div class="car-img w-100 d-flex d-flex align-items-center align-items-center">
+              <img :src="'src/assets/images/cars/' + car.year + '.jpg'" :alt="car.year" />
+            </div>
+            <div class="pt-3">
+              <h5 class="py-3">{{car.year}}</h5>
+              <p>{{car.name}}</p>
             </div>
           </div>
         </div>
@@ -50,19 +77,32 @@ import { people } from '@/assets/automation/people.json'
   background-image: url("@/assets/images/placeholders/landing-bg2.jpg");
 }
 
+#people .person > div, #cars .car > div {
+  max-width: 400px;
+  background-color: $bgColorDark;
+  border-bottom: 2px solid $primary;
+}
+
 .person-img {
   height: 200px;
   
   img {
-    height: 100%;
+    height: auto;
+    max-height: 100%;
     width: auto;
     max-width: 200px;
+    font-style: italic;
   }
 }
 
-#people .person > div {
-  max-width: 400px;
-  background-color: $bgColorDark;
-  border-bottom: 2px solid $primary;
+.car-img {
+  height: 250px;
+
+  img {
+    height: auto;
+    max-height: 100%;
+    width: auto;
+    max-width: 100%;
+  }
 }
 </style>
