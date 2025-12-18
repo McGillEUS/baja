bddcf55e98bd449bc9589ad899be2f8476df6d03
Laura <laurawang122@gmail.com>
2025-12-17 20:11:26 -0500

    title changed

--- src/views/GalleryView.vue
+++ src/views/GalleryView.vue
@@ -40,24 +40,36 @@ onMounted(async() => {
 
 <template>
   <div class="gallery-page">
-    <main id="gallery">
-      <section
-      id="gallery-landing-section"
-      class="min-vh-100 full-height-image"
-      :style="{ backgroundImage: 'url(' + landingBG + ')', backgroundAttachment: 'fixed' }"
-      >
-      <div class="full-height-overlay">
-        <div class="full-height-content landing-content" style="top: 35%">
-          <h1 class="display-1">Gallery</h1>
-          <p class="fs-5 pt-3">
-            <typing-text text="A BTS look at Baja" />
-          </p>
+    <main id="gallery" class="gallery-wrapper">
+      <div class="gallery-wrapper">
+        <section
+        id="gallery-landing-section"
+        class="min-vh-100 full-height-image"
+        :style="{ backgroundImage: 'url(' + landingBG + ')', backgroundAttachment: 'fixed' }"
+        >
+        <div class="full-height-overlay">
+          <div class="title " style="top: 35%">
+            <h1 class="display-1">Gallery</h1>
+            <h1 class="display-1 pt-3">
+              <typing-text text="A BTS look at Baja" />
+            </h1>
+          </div>
+          <span class="nav-link scroll-down" @click="anchorLink('gallery-images')"
+            ><i class="bi bi-chevron-compact-down fs-1 px-2"></i
+          ></span>
         </div>
-        <span class="nav-link scroll-down" @click="anchorLink('gallery-images')"
-          ><i class="bi bi-chevron-compact-down fs-1 px-2"></i
-        ></span>
+        </section>
+        <div class="vertical-line"></div>
       </div>
-      </section>
+
+      <div class="subtitle py-5">
+        <h2 class="display-3">Comparison View</h2>
+        <p class="fs-5 pb-5">Click and drag to compare our car to our CAD</p>
+        <compare-images
+          path1="images/gallery/Front.png"
+          path2="images/gallery/Front-cad.png"
+        />
+      </div>
 
       <section id="gallery-images" class="py-3 py-lg-5">
       <div class="text-center py-5">
@@ -65,16 +77,6 @@ onMounted(async() => {
         <div class="title-separator mt-3 mb-5 mx-auto"></div>
         <p class="fs-5 px-4">Click and drag to rotate the car</p>
         <view360 :numImages="27" :firstImage="24" path="images/gallery/360-view/" imgType="png" />
-      </div>
-
-      <div class="text-center py-5">
-        <h2 class="display-3">Comparison View</h2>
-        <div class="title-separator mt-3 mb-5 mx-auto"></div>
-        <p class="fs-5 px-4 pb-5">Click and drag to compare our car to our CAD</p>
-        <compare-images
-          path1="images/gallery/Front.png"
-          path2="images/gallery/Front-cad.png"
-        />
       </div>
 
       <div class="text-center py-5">
